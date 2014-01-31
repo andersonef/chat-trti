378f84b354e9141ad94166c460491e05c4de6510
andersonef <anderson.nuneseth@gmail.com>
2014-01-31 13:27:42 -0200

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+{
+    "name" : "MyApp",
+    "description" : "Minha App Legalzinha",
+    "dependencies" : {
+        "express" : "0.8.9",
+        "socket.io" : "0.4.6"
+    }
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <?php echo date('d/m/Y'); ?>
+    </body>
+</html>
