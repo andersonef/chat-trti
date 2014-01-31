baf07278b2acf86f5216f107277dde5aa9157049
andersonef <anderson.nuneseth@gmail.com>
2014-01-31 15:54:50 -0200

    Criado o app.js para testar

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-{
-    "name" : "MyApp",
-    "description" : "Minha App Legalzinha",
-    "dependencies" : {
-        "express" : "0.8.9",
-        "socket.io" : "0.4.6"
-    }
-}
+var io = require('socket.io');
+var socket = io.listen(8080);
+var logados = {};
+
+socket.on('connection', function (client) {
+    client.on('sendchat', function (dados) {
+        dados = JSON.parse(dados);
+        dados.lastActivity = new Date();
+        socket.Pessoa = dados;
+
+        if (!logados[dados.codigoPessoa]) {
+            dados.openModal = true;
+            logados[dados.codigoPessoa] = dados;
+        }
+        var evt = 'chat_with_' + dados.codigoPessoa;
+        client.emit(evt, JSON.stringify(dados));
+    });
+
+    client.on('disconnect', function () {
+        socket.Pessoa.lastActivity = new Date();
+        setTimeout(function () {
+            var segundos = (Date.now() - logados[socket.Pessoa.codigoPessoa].lastActivity.getTime()) / 1000;
+            if (segundos < 30) return;
+
+            delete logados[socket.Pessoa.codigoPessoa];
+            socket.Pessoa.disconected = true;
+            client.emit('sendchat', JSON.stringify(socket.Pessoa));
+        }, 30000);
+    });
+
+    client.on('senddisconnect', function (dados) {
+        dados = JSON.parse(dados);
+        var evt = 'chat_with_' + dados.codigoPessoa;
+        dados.message = dados.nomePessoa + ' ACABA DE SAIR DO BATE PAPO';
+    });
+})
